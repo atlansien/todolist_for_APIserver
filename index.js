389c79f830badb9c1f47240be9131a7e92be519e
atlansien <atlansien@yahoo.co.jp>
2019-03-31 08:52:40 +0900

    脱字修正

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const todosRouter = require('./router/todos');
 const app = express();
 const PORT = 8080;
 
-app.use('api/todos', todosRouter);
+app.use('/api/todos', todosRouter);
 
 app.listen(PORT, () =>{
     console.log(`EXample app listening on Port ${PORT}`)
