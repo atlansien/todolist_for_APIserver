dad3f1e2c18c35e0a4bac746fb665c3417e69474
atlansien <atlansien@yahoo.co.jp>
2019-03-31 10:19:30 +0900

    createメソッドの作成

--- models/Todo.js
+++ models/Todo.js
@@ -3,26 +3,43 @@ const todos = [];
 let nextId = 1;
 
 class Todo {
-    constructor({title, body}){
-        this.id = nextId++,
-        this.title = title,
-        this.body = body,
-        this.createdAt = new Date(),
-        this.updatedAt = new Date()
-    }
+  constructor({ title, body }) {
+    (this.id = nextId++),
+      (this.title = title),
+      (this.body = body),
+      (this.createdAt = new Date()),
+      (this.updatedAt = new Date());
+  }
 }
 
-for (let i = 0;i < 5; i++) {
-    const todo = new Todo({
-        title: 'タイトル' + 1,
-        body: '内容' + 1
-    });
-    todos.push(todo);
+for (let i = 0; i < 5; i++) {
+  const todo = new Todo({
+    title: "タイトル" + 1,
+    body: "内容" + 1
+  });
+  todos.push(todo);
 }
-
 
 module.exports = {
-    findAll : () => {
-        return todos;
+  findAll: () => {
+    return todos.slice();
+  },
+
+  create: ({ title, body }) => {
+    if (!title) {
+      throw new Error("titleは必須です");
     }
+    if (!body) {
+      throw new ErrorEvent("bodyは必須です");
+    }
+
+    const todo = new Todo({
+      title: title,
+      body: body
+    });
+
+    todos.push(todo);
+
+    return todo;
+  }
 };
