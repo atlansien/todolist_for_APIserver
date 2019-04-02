8038b1721dc451e03cf3eafa9647e5ec90544d6b
atlansien <atlansien@yahoo.co.jp>
2019-04-02 23:17:53 +0900

    不備なくデータを送れた場合のテスト、テストコード完成

--- test/app/api/todos/putTodo.test.js
+++ test/app/api/todos/putTodo.test.js
@@ -2,7 +2,7 @@ const assert = require("power-assert");
 
 const requestHelper = require("../../../../helper/requestHelper");
 
-const getTodo = async () => {
+const getTodos = async () => {
   const response = await requestHelper.request({
     method: "get",
     endPoint: "/api/todos",
@@ -46,5 +46,48 @@ describe("TEST 「PUT/api/todo/:id」", () => {
     assert.deepEqual(response.body, {
       message: `titleは必須です`
     });
+  });
+
+  it("bodyを送らなかった場合エラーが返る", async () => {
+    const data = { title: "test title" };
+
+    const response = await requestHelper
+      .request({
+        method: "put",
+        endPoint: `/api/todos/${VALID_ID}`,
+        statusCode: 400
+      })
+      .send(data);
+
+    assert.deepEqual(response.body, {
+      message: `bodyは必須です`
+    });
+  });
+
+  it("不備なくデータを遅れたら成功", async () => {
+    const oldTodos = await getTodos();
+
+    const data = { title: "test title", body: "test body" };
+
+    const response = await requestHelper
+      .request({
+        method: "put",
+        endPoint: `/api/todos/${VALID_ID}`,
+        statusCode: 200
+      })
+      .send(data);
+
+    const updatedTodo = response.body;
+    assert.deepEqual(updatedTodo, {
+      id: VALID_ID,
+      title: data.title,
+      body: data.body,
+      createdAt: updatedTodo.createdAt,
+      updatedAt: updatedTodo.updatedAt
+    });
+
+    const currentTodo = await getTodos();
+
+    assert.notEqual(oldTodos, currentTodo);
   });
 });
