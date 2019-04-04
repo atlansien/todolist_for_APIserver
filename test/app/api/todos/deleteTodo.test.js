e1c1def8fe6f0b818185e58019843a9cf3b71a78
atlansien <atlansien@yahoo.co.jp>
2019-04-05 00:02:39 +0900

    適切なデータが送信されたときのテスト

--- test/app/api/todos/deleteTodo.test.js
+++ test/app/api/todos/deleteTodo.test.js
@@ -25,4 +25,30 @@ describe("TEST 「DELETE /api/todos」", () => {
       message: "idに該当するtodoが存在しません"
     });
   });
+  it("適切なIDを送った場合成功する", async () => {
+    const oldTodos = await getTodos();
+
+    const response = await requestHelper.request({
+      method: "delete",
+      endPoint: `/api/todos/${VALID_ID}`,
+      statusCode: 200
+    });
+
+    const deletedTodo = response.body;
+    assert.deepEqual(deletedTodo, {
+      id: VALID_ID,
+      title: deletedTodo.title,
+      body: deletedTodo.body,
+      createdAt: deletedTodo.createdAt,
+      updatedAt: deletedTodo.updatedAt
+    });
+
+    const currentTodos = await getTodos();
+
+    assert.equal(oldTodos.length, currentTodos.length + 1);
+
+    assert.deepEqual(deletedTodo, oldTodos[0]);
+
+    assert.notDeepEqual(deletedTodo, currentTodos[0]);
+  });
 });
